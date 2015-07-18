fca9a50875f5dc194defbe2de2aaee367e20a11a
Dixon Adair <dixonadairv@gmail.com>
2015-07-17 19:08:46 -0700

    changed a few more things

--- lib/dixonadairdogeify.rb
+++ lib/dixonadairdogeify.rb
@@ -1,15 +1,21 @@
 require "dixonadairdogeify/version"
 
 module Dixonadairdogeify
+	class Hola
+    	def self.hi
+    		puts "Hello world!"
+    	end
+	end
+
 	class Array
 		def self.bark
-			"woof"	
+			puts "woof"	
 		end
 	end
 
 	class String
 		def self.bark
-			"woof woof!"			
+			puts "woof woof!"			
 		end
 	end
 end
